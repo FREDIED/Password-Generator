90f1ba44962bd4fb8c754fedce3a0f902b0805b6
FREDIED <64380397+FREDIED@users.noreply.github.com>
2020-11-24 14:51:27 +0300

    Add files via upload

---
+++ password_gen.py
@@ -0,0 +1,56 @@
+import random
+import time
+piska = '1234567890'
+piskabobra ='qwertyuiopasdfghjklzxcvbnm'
+pipiska = piskabobra.upper()
+podpiska = piska+piskabobra+pipiska
+x = (int(input("    Какая длинна? \n    ")))
+clozhna = input("   Использовать русский алфавит?\n")
+if clozhna == "n":
+    clozhna2 = input("    Использовать всякую бяку?\n")
+    if clozhna == "n":
+        piska = '1234567890'
+        piskabobra ='qwertyuiopasdfghjklzxcvbnm'
+        pipiska = piskabobra.upper()
+        podpiska = piska+piskabobra+pipiska
+        ls = list(podpiska)
+        random.shuffle(ls)
+        psw = ''.join([random.choice(ls) for x in range(x)])
+        print(psw)
+        time.sleep(10)
+    if clozna2 == "y":
+        pipirkabobra = "!@#$%^&*()_+-=?><:""}{\\|}"
+        piska = '1234567890'
+        piskabobra ='qwertyuiopasdfghjklzxcvbnm'
+        pipiska = piskabobra.upper()
+        podpiska = piska+piskabobra+pipiska+pipirkabobra
+        ls = list(podpiska)
+        random.shuffle(ls)
+        psw = ''.join([random.choice(ls) for x in range(x)])
+        print(psw)
+        time.sleep(10)
+if clozhna == "y":
+    pipirka = "йцукенгшщзхъёфывапролджэячсмитьбю"
+    clozhna2 = input("    Использовать всякую бяку?\n")
+    if clozhna == "n":
+        piska = '1234567890'
+        piskabobra ='qwertyuiopasdfghjklzxcvbnm'
+        pipiska = piskabobra.upper()
+        podpiska = piska+piskabobra+pipiska+pipirka
+        ls = list(podpiska)
+        random.shuffle(ls)
+        psw = ''.join([random.choice(ls) for x in range(x)])
+        print(psw)
+        time.sleep(10)
+    if clozhna2 == "y":
+        pipirkabobra = "!@#$%^&*()_+-=?><:""}{\\|}"
+        piska = '1234567890'
+        piskabobra ='qwertyuiopasdfghjklzxcvbnm'
+        pipiska = piskabobra.upper()
+        pipipiska = pipirka.upper()
+        podpiska = piska+piskabobra+pipiska+pipipiska+pipirkabobra
+        ls = list(podpiska)
+        random.shuffle(ls)
+        psw = ''.join([random.choice(ls) for x in range(x)])
+        print(psw)
+        time.sleep(10)
